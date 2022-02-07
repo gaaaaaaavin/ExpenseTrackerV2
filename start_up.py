511efe3e945fcaa5d75ede6bc227382c871e1446
gaaaaaaavin <59616705+gaaaaaaavin@users.noreply.github.com>
2022-02-07 14:59:25 +0530

    added geometry to window

--- start_up.py
+++ start_up.py
@@ -27,11 +27,14 @@ def set_up_page():                              # Start page
             print("no")
         else:
             print("confirmCate func error")
-
+        
+    
+    
     icon="images\icon.ico"
     startpg=Tk()
-    startpg.geometry("400x400")
+    startpg.geometry("356x253")
     startpg.iconbitmap(icon)
+    startpg.title("Quick Setup")
     
     cat0=StringVar()
     cat0.set("Clothing")
@@ -50,6 +53,8 @@ def set_up_page():                              # Start page
     cat7=StringVar()
     cat7.set("Other")
 
+    
+    
     lab1=Label(startpg,text="Before we start tracking, we've got to set a few things up").place(x=10,y=10)
     lab2=Label(startpg,text="Please type in Category titles or leave it with the default titles").place(x=10,y=30)
     catE0=Entry(startpg,width=15,textvariable=cat0).place(x=40,y=85)
@@ -63,6 +68,10 @@ def set_up_page():                              # Start page
     confirmBut=Button(startpg,width=10,text="Confirm",command=confirmCate).place(x=60,y=200)
     exitBut=Button(startpg,width=10,text="Exit",command=startpg.destroy).place(x=170,y=200)
 
+
+
+
+    
     startpg.mainloop()
 
 def mainpg():                                   # Launches main.py (main application)
